26123a9c5d812544006a6249b88c89a75faa6e8c
Destiny Moncrief <D.moncrief4@gmail.com>
2024-06-14 13:43:12 -0400

    started first exercise

---
+++ exercises/ex1.js
@@ -0,0 +1,27 @@
+"use strict";
+
+class Employee {
+  constructor(id, firstName, lastName, jobTitle, payRate) {
+    this.employeeId = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.jobTitle = jobTitle;
+    this.payRate = payRate;
+  }
+}
+
+
+let emp = new Employee()
+
+
+// let employee1 = new Employee(
+//     1, "Ian", "Auston", "Graphic Artist", 42.50);
+//    console.log(`Employee ${employee1.firstName} created`);
+
+
+
+   let employee1 = new Employee(
+    1, "Ian", "Auston", "Graphic Artist", 42.50);
+   console.log(`Employee ${employee1.fullName} created`);
+   console.log(`Job title is ${employee1.jobTitle}`);
+   console.log(`Pay rate is $${employee1.payRate}`);
